8f4940b8cd35162230ffea9b4555e321aabaee8c
Joel Cantoral <cs.joel.09@gmail.com>
2014-09-10 08:16:58 -0600

    Archivos con casos de estudio

---
+++ scripts/analize/Casos.R
@@ -0,0 +1,10 @@
+MontoSumAvgByClient<-function()
+{
+  # Field: e.g. C87503
+  # Fieldname: e.g. "monto"
+  moneyDf <- data.frame(trx_data[,c("C87503","C87584")])
+  names(moneyDf)[1] <- "monto"
+  names(moneyDf)[2] <- "idCliente"
+  promVentaCliente <- ddply(moneyDf,~idCliente,summarise,mean=mean(monto),sum=sum(monto))
+  return(promVentaCliente)
+}
