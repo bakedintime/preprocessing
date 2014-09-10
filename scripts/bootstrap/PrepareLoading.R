e42a31da92fbda960a495c5e164f12f4cb601652
Joel Cantoral <cs.joel.09@gmail.com>
2014-09-10 08:03:43 -0600

    Se agregó el paquete plyr para agrupación de datos

--- scripts/bootstrap/PrepareLoading.R
+++ scripts/bootstrap/PrepareLoading.R
@@ -3,7 +3,9 @@ PrepareLoading<-function()
   EnsurePackage("bit")
   EnsurePackage("ff")
   EnsurePackage("ffbase")
+  EnsurePackage("plyr")
   
   library("ff")
   library("ffbase")
+  library("plyr")
 }
